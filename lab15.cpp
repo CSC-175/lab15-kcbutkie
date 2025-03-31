4b41d43a1f2b38cca4dba3862296025d7afa6bbc
Kace Butkiewicz <kcbutkie@umich.edu>
2025-03-31 17:14:40 -0400

    Attempt 1

--- lab15.cpp
+++ lab15.cpp
@@ -12,7 +12,23 @@ double factorial(int);
 * reference parameters referencing variables in the main function. *
 ********************************************************************/
 
+void getInfo(int& num1, int& num2) {
+cout << "How many balls (1-12) are in the pool to pick from? ";
+    cin >> num1;
+    while ((num1 > 12)){
+        cout << "Input error! There must be between 1 and 12 balls." << endl;
+        cout << "How many balls (1-12) are in the pool to pick from? " << endl;
+        cin >> num1;
+    }
+    cout << "How many balls (1-" << num1 << ") will be drawn? ";
+    cin >> num2;
+    while (num2 > num1) {
+        cout << "Input error!" << endl;
+        cout << "How many balls (1-" << num1 << ") will be drawn? ";
+        cin >> num2;
+    }
 
+}
 
 /*******************************************************************
 * computeWays                                                      *
@@ -26,10 +42,20 @@ double factorial(int);
 * intermediate result becoming so large that it causes overflow.   *
 ********************************************************************/
 
+double computeWays (int n, int k) {
+    return (factorial(k)*factorial(n-k))/(factorial(n));
+}
 
 /*******************************************************************
 * factorial                                                        *
 * This function computes factorials recursively.                   *
 *******************************************************************/
 
-
+double factorial(int number) {
+    if (number == 0) { //referred to as base case; we know the result
+        return 1;
+    }
+    else {
+        return number * factorial(number - 1);
+    }
+}
